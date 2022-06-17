d3ce279af539fc78eb65fc114c63e506bb801fe7
Alexander-gMa <82538974+Alexander-gMa@users.noreply.github.com>
2022-06-17 18:45:06 +0300

    feat: add catch for syntax error

--- src/Errors/Handler.error.ts
+++ src/Errors/Handler.error.ts
@@ -6,6 +6,14 @@ const HandleError: RouterCallbackFunc = (req, res, err) => {
     if (err instanceof BaseError) {
         res.statusCode = err.code;
         res.end(err.message);
+    }
+    else if (err instanceof SyntaxError) {
+        res.statusCode = 400;
+        res.end(`
+        There is a SyntaxError
+        There are the possible reasons:
+        1)Unexpected token } in JSON -> Remove last comma
+        2)Wrong comma in hobbies, replace single quotes with double quotes`);
     } else {
         const { code, message } = new ServerInternalError();
         res.statusCode = code;
