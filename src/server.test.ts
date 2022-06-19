af726f07e6fe5395d168657c16a53b932e6d1d4b
Alexander-gMa <82538974+Alexander-gMa@users.noreply.github.com>
2022-06-19 16:06:42 +0300

    feat: add implement tests for first scenario

--- src/server.test.ts
+++ src/server.test.ts
@@ -6,32 +6,77 @@ import { BASE_URL } from './utils/constants';
 
 const server = createServer();
 
-const USER: IUser = {
-    username: 'Alesha Popovich',
+let USER: IUser = {
+    username: "Alesha Popovich",
     age: 666,
     hobbies: ["fishing", "running"]
 };
 
-describe('Testing CRUD API', () => {
+const SECOND_USER: IUser = {
+    username: "Mitya Fomin",
+    age: 19,
+    hobbies: ["signing", "dancing"]
+}
+
+describe('Simple CRUD API TEST', () => {
     describe('First scenario', () => {
+        let id: number = 0;
         afterAll(() => server.close());
         it('Should return all users', async () => {
-            const { body, statusCode } = await request(server)
-                .get(BASE_URL).set('Accept', 'application/json');
-            expect(statusCode).toEqual(200);
-            expect(body).toEqual([]);
-            expect(Array.isArray(body)).toBe(true);
+            const requestServer = await request(server)
+                .get(BASE_URL)
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/);
+            expect(requestServer.statusCode).toEqual(200);
+            expect(requestServer.body).toEqual([]);
+            expect(Array.isArray(requestServer.body)).toBe(true);
         });
-        it('Should create user', async () => {
-            const { body, statusCode } = await request(server)
+        it('Should create user successfully', async () => {
+            const requestServer = await request(server)
                 .post(BASE_URL)
-                .send(USER);
-            expect(statusCode).toEqual(201);
-            expect(body.username).toEqual(USER.username);
-            expect(body.age).toEqual(USER.age);
-            expect(JSON.stringify(body.hobbies)).toEqual(
-                JSON.stringify(USER.hobbies),
-            );
+                .send(USER)
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/);
+            expect(requestServer.statusCode).toEqual(201);
+            expect(requestServer.body.username).toEqual(USER.username);
+            expect(requestServer.body.age).toEqual(USER.age);
+            expect(JSON.stringify(requestServer.body.hobbies))
+                .toEqual(
+                    JSON.stringify(USER.hobbies),
+                );
+            id = requestServer.body.id;
+        });
+        it('Should get a person by id', async () => {
+            const requestServer = await request(server)
+                .get(`${BASE_URL}/${id}`)
+            expect(requestServer.statusCode).toEqual(200);
+            expect(requestServer.body).toEqual({ ...USER, id });
+            expect(requestServer.body instanceof Object).toBe(true);
+        });
+        it('Should update a person by id', async () => {
+            USER = { ...USER, username: "Mity Fomin" };
+            const requestServer = await request(server)
+                .put(`${BASE_URL}/${id}`)
+                .send(USER)
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+            expect(requestServer.statusCode).toEqual(200);
+            expect(requestServer.body.username).toEqual(USER.username);
+            expect(requestServer.body.age).toEqual(USER.age);
+            expect(JSON.stringify(requestServer.body.hobbies))
+                .toEqual(
+                    JSON.stringify(USER.hobbies),
+                );
+        });
+        it('Should delete a person by id', async () => {
+            const requestServer = await request(server)
+                .delete(`${BASE_URL}/${id}`)
+            expect(requestServer.statusCode).toEqual(204);
+        });
+        it('Should return error 404, because user is deleted', async () => {
+            const requestServer = await request(server)
+                .get(`${BASE_URL}/${id}`)
+            expect(requestServer.statusCode).toEqual(404);
         });
     });
 })
